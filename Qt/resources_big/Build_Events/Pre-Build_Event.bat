67b9ad668cc5708d519420a111d7321c8adb3a83
OlegKorzun <OKorzun@ru.netnucleus.com>
2021-11-24 15:32:28 +0300

    1

--- Qt/resources_big/Build_Events/Pre-Build_Event.bat
+++ Qt/resources_big/Build_Events/Pre-Build_Event.bat
@@ -4,7 +4,7 @@
 
 IF NOT '%ERRORLEVEL%'=='0' GOTO failure
 
-"%~2\cl.exe" /c "%~dp0..\qrc_BigResource1.cpp" /Fo:"%~dp0..\qrc_BigResource1.tmp.obj"
+"%~2\cl.exe" /c "%~dp0..\qrc_BigResource1.cpp" /MP /GS /analyze- /W1 /Zc:wchar_t /Zi /Gm- /Od /WX- /Zc:forScope /Gd /Oy- /MTd /FC /EHsc /nologo /Fo:"%~dp0..\qrc_BigResource1.tmp.obj"
 
 IF NOT '%ERRORLEVEL%'=='0' GOTO failure
 
@@ -16,3 +16,5 @@ EXIT /B 0
 
 :failure
 EXIT /B 1
+
+REM /MP /GS /analyze- /W1 /Zc:wchar_t /I"D:\qt\5.14.2.0.m\include" /I"D:\qt\5.14.2.0.m\include\QtWidgets" /I"D:\qt\5.14.2.0.m\include\QtGui" /I"D:\qt\5.14.2.0.m\include\QtCore" /I"D:\qt\5.14.2.0.m\mkspecs\win32-msvc" /I"Debug\moc" /I"Debug\uic" /Zi /Gm- /Od /Fd"Debug\vc141.pdb" /Zc:inline /fp:precise /D "UNICODE" /D "_UNICODE" /D "WIN32" /D "_ENABLE_EXTENDED_ALIGNED_STORAGE" /D "QT_WIDGETS_LIB" /D "QT_GUI_LIB" /D "QT_CORE_LIB" /errorReport:prompt /WX- /Zc:forScope /Gd /Oy- /MTd /FC /Fa"Debug\" /EHsc /nologo /Fo"Debug\" /Fp"Debug\BigResource1.pch" /diagnostics:classic 
