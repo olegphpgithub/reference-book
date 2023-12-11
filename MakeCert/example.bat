4635d9f57bf022926c9febba478523adca09328d
OlegKorzun <OKorzun@ru.netnucleus.com>
2023-12-11 17:55:22 +0300

    Fix in example.bat file;

--- MakeCert/example.bat
+++ MakeCert/example.bat
@@ -1,2 +1,2 @@
-"%~dp0MakeCert.exe" -r -pe -n "CN=Your company" -a sha256 -cy end -sky signature -sv company.pvk company.cer -b 04/30/2023 -e 04/30/2026
+"%~dp0MakeCert.exe" -r -pe -n "CN=\"Your company\"" -a sha256 -cy end -sky signature -sv company.pvk company.cer -b 04/30/2023 -e 04/30/2026
 "%~dp0pvk2pfx.exe" -pvk company.pvk -spc company.cer -pfx company.pfx
