cef9ef7b283dd2f9ece72bf5a4dac52497889336
OlegKorzun <OKorzun@ru.netnucleus.com>
2024-11-20 12:30:21 +0300

    Add second method;

--- Helper/create_repack/create_repack.py
+++ Helper/create_repack/create_repack.py
@@ -1,13 +1,13 @@
 import os
 import re
 import json
+import math
 import argparse
 import shutil
 from collections import OrderedDict
 from os import listdir
 from os.path import isfile, join
 from pprint import pprint
-
 
 
 class Unit:
@@ -24,7 +24,7 @@ class CloneFile:
         self.dst = d
 
 
-def create_repack(src_dir, dst_dir, cpy_mth):
+def create_repack(src_dir, dst_dir, cpy_mth, file_qnt):
 
     if src_dir == dst_dir:
         raise Exception(r'Source and destination directories are the same.')
@@ -51,7 +51,7 @@ def create_repack(src_dir, dst_dir, cpy_mth):
 
     if cpy_mth == r'rotation':
         counter = 1
-        while counter < 200 + 1:
+        while counter < file_qnt + 1:
             for src_unit in src_units:
                 src_path = join(src_dir, src_unit.prefix)
                 src_path += r'_' + src_unit.number + r'_' + src_unit.param + r'.' + src_unit.ext
@@ -59,10 +59,23 @@ def create_repack(src_dir, dst_dir, cpy_mth):
                 dst_path += r'_' + f'{counter:03}' + r'_' + src_unit.param + r'.' + src_unit.ext
                 copy_map[counter] = CloneFile(src_path, dst_path)
                 counter = counter + 1
-                if counter >= 200:
+                if counter >= file_qnt:
                     break
     elif cpy_mth == r'together':
-        pass
+        counter = 1
+        bunch = math.ceil(file_qnt / len(src_units))
+        for src_unit in src_units:
+            for i in range(bunch):
+                src_path = join(src_dir, src_unit.prefix)
+                src_path += r'_' + src_unit.number + r'_' + src_unit.param + r'.' + src_unit.ext
+                dst_path = join(dst_dir, src_unit.prefix)
+                dst_path += r'_' + f'{counter:03}' + r'_' + src_unit.param + r'.' + src_unit.ext
+                copy_map[counter] = CloneFile(src_path, dst_path)
+                if counter >= file_qnt:
+                    break
+                counter = counter + 1
+            if counter >= file_qnt:
+                break
 
     for copy_item in copy_map.values():
         print(copy_item.src)
@@ -70,16 +83,15 @@ def create_repack(src_dir, dst_dir, cpy_mth):
         shutil.copyfile(copy_item.src, copy_item.dst)
         print()
 
-    # onlyfiles = [f for f in listdir(src_dir) if isfile(join(src_dir, f))]
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source-directory', required=True)
     parser.add_argument('--destination-directory', required=True)
     parser.add_argument('--copy-method', required=True)
+    parser.add_argument('--file-quantity', required=True)
     args = parser.parse_args()
 
-    create_repack(args.source_directory, args.destination_directory, args.copy_method)
+    create_repack(args.source_directory, args.destination_directory, args.copy_method, int(args.file_quantity))
 
     pass
