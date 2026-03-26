57ea69b16d5f10d20a896128dd7104599c146b00
OlegKorzun <OKorzun@ru.netnucleus.com>
2026-03-26 18:02:38 +0300

    chatgpt;

---
+++ VPN/chatgpt/delete.bat
@@ -0,0 +1,8 @@
+rem cdn.openai.com
+rem chatgpt.com
+
+route delete 172.64.155.0 mask 255.255.255.0 192.168.11.144
+route delete 8.6.112.0 mask 255.255.255.0 192.168.11.144
+route delete 8.47.69.0 mask 255.255.255.0 192.168.11.144
+route delete 13.107.253.0 mask 255.255.255.0 192.168.11.144
+route delete 13.107.226.0 mask 255.255.255.0 192.168.11.144
